81cc2b5ff18f0bf29e5801d733782744cd780c73
Helldragger <christopher1223334444@live.fr>
2020-07-13 00:21:27 +0200

    mod initial upload

---
+++ Source/HeatsinkStatistics.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace SOS2HS
+{
+    /* First step: see the patch which adds data to the xml comp data
+     *
+     * Second step of this mod: adding this
+     * Displaying this information
+    */
+
+    [StaticConstructorOnStartup]
+    public static class HeatsinkStatistics
+    {
+        public static float GetMaxHeatPushed()
+        {
+            // modified from source : https://stackoverflow.com/questions/2665648/how-do-i-get-class-of-an-internal-static-class-in-another-assembly
+            var ass = Assembly.GetAssembly(typeof(ShipCombatLaserMote));
+            var type = ass.GetType("RimWorld.ShipCombatManager");
+            var prop = type.GetField("HeatPushMult");
+            return (float)prop.GetValue(type);
+        }
+
+        public static float GetMaxHeatOutput(StatRequest req, bool applyPostProcess = true)
+        {
+            if (!(req.Thing is Thing))
+            {
+                return 0f;
+            }
+
+            // modified from source : https://stackoverflow.com/questions/2665648/how-do-i-get-class-of-an-internal-static-class-in-another-assembly
+            var ass = Assembly.GetAssembly(typeof(ShipCombatLaserMote));
+            var type = ass.GetType("RimWorld.ShipCombatManager");
+            var prop = type.GetField("HeatPushMult");
+            var heatPushed = (float)prop.GetValue(type) / GetHeatVentTick(req, applyPostProcess);
+            RoomGroup roomGroup = req.Thing.Position.GetRoomGroup(req.Thing.Map);
+            if (roomGroup != null && !roomGroup.UsesOutdoorTemperature)
+            {
+                return heatPushed / roomGroup.CellCount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static float GetMaxHeatOutputPerSecond(StatRequest req, bool applyPostProcess = true)
+        {
+            return GetMaxHeatOutput(req, applyPostProcess) * 60;
+        }
+
+        public static float GetCurrentCoolingPerSecond(StatRequest req, bool applyPostProcess = true)
+        {
+            if (!(req.Thing is Building_TempControl))
+            {
+                return 0f;
+            }
+
+            Building_TempControl radiator = (Building_TempControl)req.Thing;
+
+            IntVec3 intVec3_1 = radiator.Position + IntVec3.North.RotatedBy(radiator.Rotation);
+            IntVec3 intVec3_2 = radiator.Position + IntVec3.South.RotatedBy(radiator.Rotation);
+
+            float cooledRoomTemp = intVec3_1.GetTemperature(radiator.Map);
+
+            float extRoomTemp = intVec3_2.GetTemperature(radiator.Map);
+
+            float roomTempGradient = (cooledRoomTemp - extRoomTemp);
+
+            float efficiencyLossPerDegree = 1.0f / 130.0f; // SOS2 internal value, means loss of efficiency for each degree above targettemp, lose 50% at 65C above targetTemp, 100% at 130+
+
+            float efficiency = (1f - roomTempGradient * efficiencyLossPerDegree);
+
+            List<CompProperties_TempControl> comps = radiator.def.comps.Where(cp => cp is CompProperties_TempControl).Select(cp => cp as CompProperties_TempControl).ToList();
+            if (comps.Count != 1)
+            {
+                Log.Error(radiator.Label + " have 0 or more than 1 CompProperties_TempControl comps, this is a bug, report to the dev");
+                // BUG
+                return 0f;
+            }
+            float energyPerSecond = comps[0].energyPerSecond; // the power of the radiator
+
+            RoomGroup roomGroup = intVec3_1.GetRoomGroup(req.Thing.Map);
+            float roomSurface = 0f; // the power of the radiator
+            if (roomGroup != null && !roomGroup.UsesOutdoorTemperature)
+            {
+                roomSurface = roomGroup.CellCount;
+            }
+
+            float unknownConstant = 4.16666651f; // Celsius cooled per Joules*Second*Meter^2  conversion rate
+
+            float maxCooling = energyPerSecond * efficiency / roomSurface * unknownConstant; // max cooling power possible
+
+            float targetTemp = radiator.compTempControl.targetTemperature;
+
+            float coolingNeeded = targetTemp - cooledRoomTemp;
+
+            float actualCoolingPerSecond = Mathf.Min(Mathf.Max(coolingNeeded, maxCooling), 0) * 60;
+
+            return actualCoolingPerSecond;
+        }
+
+        public static float GetMaxCoolingPerSecond(StatRequest req, bool applyPostProcess = true)
+        {
+            if (!(req.Thing is Building_TempControl))
+            {
+                return 0f;
+            }
+
+            Building_TempControl radiator = (Building_TempControl)req.Thing;
+
+            IntVec3 intVec3_1 = radiator.Position + IntVec3.North.RotatedBy(radiator.Rotation);
+            IntVec3 intVec3_2 = radiator.Position + IntVec3.South.RotatedBy(radiator.Rotation);
+
+            float cooledRoomTemp = intVec3_1.GetTemperature(radiator.Map);
+
+            float extRoomTemp = intVec3_2.GetTemperature(radiator.Map);
+
+            float roomTempGradient = (cooledRoomTemp - extRoomTemp);
+
+            float efficiencyLossPerDegree = 1.0f / 130.0f; // SOS2 internal value, means loss of efficiency for each degree above targettemp, lose 50% at 65C above targetTemp, 100% at 130+
+
+            float efficiency = (1f - roomTempGradient * efficiencyLossPerDegree);
+
+            List<CompProperties_TempControl> comps = radiator.def.comps.Where(cp => cp is CompProperties_TempControl).Select(cp => cp as CompProperties_TempControl).ToList();
+            if (comps.Count != 1)
+            {
+                Log.Error(radiator.Label + " have 0 or more than 1 CompProperties_TempControl comps, this is a bug, report to the dev");
+                // BUG
+                return 0f;
+            }
+            float energyPerSecond = comps[0].energyPerSecond; // the power of the radiator
+
+            RoomGroup roomGroup = intVec3_1.GetRoomGroup(req.Thing.Map);
+            float roomSurface = 0f; // the power of the radiator
+            if (roomGroup != null && !roomGroup.UsesOutdoorTemperature)
+            {
+                roomSurface = roomGroup.CellCount;
+            }
+
+            float unknownConstant = 4.16666651f; // Celsius cooled per Joules*Second*Meter^2  conversion rate
+
+            float maxCoolingPerSecond = energyPerSecond * efficiency / roomSurface * unknownConstant * 60; // max cooling power possible
+
+            return maxCoolingPerSecond;
+        }
+
+        public static float GetHeatVentTick(StatRequest req, bool applyPostProcess = true)
+        {
+            List<CompProperties_ShipHeat> shipHeat = req.Thing.def.comps.Where(cp => cp is CompProperties_ShipHeat).Select(cp => cp as CompProperties_ShipHeat).ToList();
+            if (shipHeat.Count == 0)
+            {
+                // something is wrong here, there is nothing
+                return 1;
+            }
+            if (shipHeat.Count > 1)
+            {
+                //there is a duplicate, that s wrong too
+                return 1;
+            }
+            return shipHeat[0].heatVentTick;
+        }
+
+        public static int GetHeatVentTick(ThingDef def)
+        {
+            /*
+             * Learned tip:
+             * those three methods allows to fetch the same things from iterables
+             * using 3 different ways
+             */
+
+            /* the loop way
+            foreach (CompProperties cp in def.comps)
+            {
+                if (cp is CompProperties_ShipHeat shipheat)
+                {
+                    Log.Message($"{shipheat.heatVentTick} heat vent tick (for loop)");
+                }
+            }
+            */
+
+            /* The linq way (sql like query)
+            IEnumerable<CompProperties_ShipHeat> shipHeatslinq =
+                from cp in def.comps
+                where cp is CompProperties_ShipHeat
+                select cp as CompProperties_ShipHeat;
+            foreach (CompProperties_ShipHeat cp in shipHeatslinq)
+            {
+                Log.Message($"{cp.heatVentTick} heat vent tick (linq)");
+            }
+            */
+
+            /* The functional way
+            IEnumerable<CompProperties_ShipHeat> shipHeatsfunc = def.comps.Where(cp => cp is CompProperties_ShipHeat).Select(cp=> cp as CompProperties_ShipHeat);
+            foreach (CompProperties_ShipHeat cp in shipHeatsfunc)
+            {
+                Log.Message($"{cp.heatVentTick} heat vent tick (functional)");
+            }
+            */
+
+            //but I love functional so..
+            List<CompProperties_ShipHeat> shipHeat = def.comps.Where(cp => cp is CompProperties_ShipHeat).Select(cp => cp as CompProperties_ShipHeat).ToList();
+            if (shipHeat.Count == 0)
+            {
+                // something is wrong here, there is nothing
+                return 1;
+            }
+            if (shipHeat.Count > 1)
+            {
+                //there is a duplicate, that s wrong too
+                return 1;
+            }
+            return shipHeat[0].heatVentTick;
+        }
+
+        static HeatsinkStatistics()
+        {
+        }
+    }
+}
---
+++ Source/StatWorker_CurrentCoolingPerSecond.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace SOS2HS
+{
+    public class StatWorker_CurrentCoolingPerSecond : StatWorker
+    {
+        public override bool IsDisabledFor(Thing thing)
+        {
+            if (!base.IsDisabledFor(thing))
+            {
+                return !thing.def.comps
+                    .Where(cp => cp is CompProperties_ShipHeat)
+                    .Select(cp => cp as CompProperties_ShipHeat)
+                    .Any(cp => cp.ventHeatToSpace);
+            }
+            return true;
+        }
+
+        public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
+        {
+            if (req.Thing == null)
+            {
+                Log.Error(string.Concat("Getting CurrentCoolingPerSecond stat for ", req.Def, " without concrete thing. This always returns 1."));
+                return 1;
+            }
+            return HeatsinkStatistics.GetCurrentCoolingPerSecond(req, applyPostProcess);
+        }
+
+        public override string GetExplanationUnfinalized(StatRequest req, ToStringNumberSense numberSense)
+        {
+            List<CompProperties_TempControl> TempControl = req.Thing.def.comps
+                .Where(cp => cp is CompProperties_TempControl)
+                .Select(cp => cp as CompProperties_TempControl)
+                .ToList();
+            if (TempControl.Count == 0)
+            {
+                Log.Error("No temperature control found, this is a bug, report to the dev");
+                // something is wrong here, there is nothing
+                return "No temperature control found, this is a bug, report to the dev";
+            }
+            if (TempControl.Count > 1)
+            {
+                Log.Error("Duplicate temperature control found, this is a bug, report to the dev");
+                //there is a duplicate, that s wrong too
+                return "Duplicate temperature control found, this is a bug, report to the dev";
+            }
+
+            if (!(req.Thing is Building_TempControl))
+            {
+                return req.Thing.Label + "is no Building_TempControl, this is a bug, report to the dev";
+            }
+            Building_TempControl radiator = (Building_TempControl)req.Thing;
+
+            IntVec3 intVec3_1 = radiator.Position + IntVec3.North.RotatedBy(radiator.Rotation);
+            IntVec3 intVec3_2 = radiator.Position + IntVec3.South.RotatedBy(radiator.Rotation);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            float cooledRoomTemp = intVec3_1.GetTemperature(radiator.Map);
+            stringBuilder.AppendLine("StatsReport_SOS2HS_CooledRoomTemp".Translate());
+            stringBuilder.AppendLine("  " + cooledRoomTemp + " C");
+
+            float extRoomTemp = intVec3_2.GetTemperature(radiator.Map);
+            stringBuilder.AppendLine("StatsReport_SOS2HS_ExteriorRoomTemp".Translate());
+            stringBuilder.AppendLine("  " + extRoomTemp + " C");
+
+            float targetTemp = radiator.compTempControl.targetTemperature;
+            stringBuilder.AppendLine("StatsReport_SOS2HS_TargetTemperature".Translate());
+            stringBuilder.AppendLine("  " + targetTemp + " C");
+
+            float efficiencyLossPerDegree = 1.0f / 130.0f; // SOS2 internal value, means loss of efficiency for each degree above targettemp, lose 50% at 65C above targetTemp, 100% at 130+
+            stringBuilder.AppendLine("StatsReport_SOS2HS_EfficiencyLossPerDegree".Translate());
+            stringBuilder.AppendLine("  " + efficiencyLossPerDegree + " C^-1");
+
+            float energyPerSecond = TempControl[0].energyPerSecond; // the power of the radiator
+            stringBuilder.AppendLine("StatsReport_SOS2HS_EnergyPerSecond".Translate());
+            stringBuilder.AppendLine("  " + energyPerSecond + " J.s^-1");
+
+            RoomGroup roomGroup = intVec3_1.GetRoomGroup(radiator.Map);
+            float roomSurface = 0f; // the power of the radiator
+            stringBuilder.AppendLine("StatsReport_SOS2HS_CooledRoomSurface".Translate());
+            if (roomGroup != null && !roomGroup.UsesOutdoorTemperature)
+            {
+                roomSurface = roomGroup.CellCount;
+                stringBuilder.AppendLine("  " + roomSurface + " m^2");
+            }
+            else
+            {
+                stringBuilder.AppendLine("  ? m^2 (outdoor)");
+            }
+
+            float coolingConversionRate = 4.16666651f; // Celsius cooled per Joules*Second*Meter^2  conversion rate
+            stringBuilder.AppendLine("StatsReport_SOS2HS_CoolingConversionRate".Translate());
+            stringBuilder.AppendLine("  " + coolingConversionRate + " C.J^-1.s^-1.m^-2");
+
+            float targetTempDiff = targetTemp - cooledRoomTemp;
+            stringBuilder.AppendLine("StatsReport_SOS2HS_TargetTempDiff".Translate());
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_TargetTempDiffEquation".Translate() + " )");
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_TargetTempDiffEquationNumbers".Translate(targetTemp, cooledRoomTemp) + " )");
+            stringBuilder.AppendLine("  " + targetTempDiff + " C");
+
+            float sidesTempGradient = (cooledRoomTemp - extRoomTemp);
+            stringBuilder.AppendLine("StatsReport_SOS2HS_SidesTempGradient".Translate());
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_SidesTempGradientEquation".Translate() + " )");
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_SidesTempGradientEquationNumbers".Translate(cooledRoomTemp, extRoomTemp) + " )");
+            stringBuilder.AppendLine("  " + sidesTempGradient + " C");
+
+            float efficiency = (1f - sidesTempGradient * efficiencyLossPerDegree);
+            stringBuilder.AppendLine("StatsReport_SOS2HS_Efficiency".Translate());
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_EfficiencyEquation".Translate() + " )");
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_EfficiencyEquationNumbers".Translate(sidesTempGradient, efficiencyLossPerDegree) + " )");
+            stringBuilder.AppendLine("  " + (efficiency * 100) + " %");
+
+            float maxCooling = energyPerSecond * efficiency / roomSurface * coolingConversionRate; // max cooling power possible
+            stringBuilder.AppendLine("StatsReport_SOS2HS_MaxCooling".Translate());
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_MaxCoolingEquation".Translate() + " )");
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_MaxCoolingEquationNumbers".Translate(energyPerSecond, efficiency, roomSurface, coolingConversionRate) + " )");
+            stringBuilder.AppendLine("  " + maxCooling + " C");
+
+            float actualCooling = Mathf.Min(Mathf.Max(targetTempDiff, maxCooling), 0) * 60;
+            stringBuilder.AppendLine("StatsReport_SOS2HS_ActualCoolingPerSecond".Translate());
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_ActualCoolingPerSecondEquation".Translate() + " )");
+            stringBuilder.AppendLine("  (= " + "StatsReport_SOS2HS_ActualCoolingPerSecondEquationNumbers".Translate(targetTempDiff, maxCooling) + " )");
+            stringBuilder.AppendLine("  " + actualCooling + " C.s^-1");
+
+            return stringBuilder.ToString();
+        }
+
+        public override string GetStatDrawEntryLabel(StatDef stat, float value, ToStringNumberSense numberSense, StatRequest optionalReq, bool finalized = true)
+        {
+            return string.Format("{0} C", HeatsinkStatistics.GetCurrentCoolingPerSecond(optionalReq).ToString("0.###"));
+        }
+
+        public override bool ShouldShowFor(StatRequest req)
+        {
+            if (base.ShouldShowFor(req))
+            {
+                if (!req.HasThing)
+                {
+                    return false;
+                }
+                return req.Thing.def.comps
+                    .Where(cp => cp is CompProperties_ShipHeat)
+                    .Select(cp => cp as CompProperties_ShipHeat)
+                    .Any(cp => cp.ventHeatToSpace);
+            }
+            return false;
+        }
+
+        public override IEnumerable<Dialog_InfoCard.Hyperlink> GetInfoCardHyperlinks(StatRequest statRequest)
+        {
+            yield return new Dialog_InfoCard.Hyperlink();
+        }
+    }
+}
